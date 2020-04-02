94f7c1466aed16ec869f46862417799d065c8792
Kamil Owczarz <kamilo2602@gmail.com>
2020-04-02 23:47:32 +0200

    Setup Dagger, add navigation

---
+++ app/src/main/java/pl/edu/pk/mobile/tourtool/fragments/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package pl.edu.pk.mobile.tourtool.fragments
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class LoginViewModel @Inject constructor() : ViewModel() {
+  // TODO: Implement the ViewModel
+}
