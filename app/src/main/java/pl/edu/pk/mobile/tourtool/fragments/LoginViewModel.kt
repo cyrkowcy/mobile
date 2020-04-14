02722ee55f86992cedff43cb7e01d903bf769017
Kamil Owczarz <kamilo2602@gmail.com>
2020-04-14 20:57:42 +0200

    Master fixes

--- app/src/main/java/pl/edu/pk/mobile/tourtool/fragments/LoginViewModel.kt
+++
@@ -1,8 +0,0 @@
-package pl.edu.pk.mobile.tourtool.fragments
-
-import androidx.lifecycle.ViewModel
-import javax.inject.Inject
-
-class LoginViewModel @Inject constructor() : ViewModel() {
-  // TODO: Implement the ViewModel
-}
