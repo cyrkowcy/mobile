96b2cd8c1667147e9ed2a41a7c42437e5f1ec03e
Kamil Owczarz <kamilo2602@gmail.com>
2020-06-08 22:27:14 +0200

    #9 implement user repository (#34)
    * Add user login function in UserRepositoryImplementation
    
    * Update Webservice
    
    * Implement user repository

--- app/src/main/java/pl/edu/pk/mobile/tourtool/di/module/ApplicationModule.kt
+++ app/src/main/java/pl/edu/pk/mobile/tourtool/di/module/ApplicationModule.kt
@@ -4,7 +4,7 @@ import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 import kotlinx.coroutines.Dispatchers
-import pl.edu.pk.mobile.tourtool.service.data.MockUserRepository
+import pl.edu.pk.mobile.tourtool.service.data.UserRepositoryImplementation
 import pl.edu.pk.mobile.tourtool.service.repositories.SharedPreferencesRepository
 import pl.edu.pk.mobile.tourtool.service.repositories.UserRepository
 import pl.edu.pk.mobile.tourtool.util.SharedPreferencesHolder
@@ -20,17 +20,17 @@ object ApplicationModule {
   @Provides
   fun provideIoDispatcher() = Dispatchers.IO
 
-//  @JvmStatic
-//  @Singleton
-//  @Provides
-//  fun provideUserRepository(userRepositoryImplementation: UserRepositoryImplementation): UserRepository =
-//    userRepositoryImplementation
-
   @JvmStatic
   @Singleton
   @Provides
-  fun provideUserRepository(mockUserRepository: MockUserRepository): UserRepository =
-    mockUserRepository
+  fun provideUserRepository(userRepositoryImplementation: UserRepositoryImplementation): UserRepository =
+    userRepositoryImplementation
+
+//  @JvmStatic
+//  @Singleton
+//  @Provides
+//  fun provideUserRepository(mockUserRepository: MockUserRepository): UserRepository =
+//    mockUserRepository
 
   @JvmStatic
   @Singleton
--- app/src/main/java/pl/edu/pk/mobile/tourtool/retrofit/Webservice.kt
+++ app/src/main/java/pl/edu/pk/mobile/tourtool/retrofit/Webservice.kt
@@ -1,18 +1,28 @@
 package pl.edu.pk.mobile.tourtool.retrofit
 
 import com.auth0.android.jwt.JWT
+import pl.edu.pk.mobile.tourtool.service.dto.CredentialsDTO
+import pl.edu.pk.mobile.tourtool.service.dto.UserDTO
+import pl.edu.pk.mobile.tourtool.service.model.User
+import pl.edu.pk.mobile.tourtool.service.model.UserResponse
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface Webservice {
-  @POST("/login")
-  @FormUrlEncoded
+  @POST("login")
   fun login(
-    @Field("email")
-    email: String,
-    @Field("password")
-    password: String
+    @Body credentialsDTO: CredentialsDTO
   ): Call<JWT>
+
+  @GET("user")
+  fun getUser(): Call<User>
+
+  @POST("user")
+  fun postUser(@Body user: UserDTO): Call<UserResponse>
+
+  @PATCH("user/{email}")
+  fun patchUser(@Body user: UserDTO, email: String): Call<UserResponse>
 }
